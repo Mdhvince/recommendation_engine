a5054bf72ff1a7ff86551d2b96d71c54fa1ae9f0
mdhvince <vinceslas.medhy@gmail.com>
2023-10-22 18:12:59 +0200

    Add user-item indexer class

--- src/recommender.py
+++ src/recommender.py
@@ -1,25 +1,20 @@
 import json
 import configparser
-import pickle
-from collections import namedtuple
 from pathlib import Path
 from typing import Dict
 
 import torch
-import numpy as np
+
+from src.user_item_indexer import UserItemIndexer
 
 
 class Recommender:
     def __init__(self, config):
         self.cfg_data = config["DATA"]
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.unseen_mode_nan = self.cfg_data.get("unseen_mode") == "nan"
+        self.ui_indexer = UserItemIndexer(config)
 
-        (self.mean_ratings,
-         self.user_factors, self.item_factors,
-         self.user_bias, self.item_bias,
-         self.u_name_to_idx, self.it_name_to_idx) = self.load()
-
+        self.mean_ratings, self.user_factors, self.item_factors, self.user_bias, self.item_bias = self.load()
         print(f"\nRunning on {self.device}.\n")
 
     def load(self):
@@ -30,7 +25,6 @@ class Recommender:
         user_bias_path = data_inference_dir / self.cfg_data.get("user_bias_filename")
         item_bias_path = data_inference_dir / self.cfg_data.get("item_bias_filename")
         stats_path = data_inference_dir / self.cfg_data.get("stats_filename")
-        interactions_path = data_inference_dir / self.cfg_data.get("interactions_filename")
 
         user_factors_mat: torch.FloatTensor = torch.load(user_factors_path, map_location=self.device)
         item_factors_mat: torch.FloatTensor = torch.load(item_factors_path, map_location=self.device)
@@ -41,17 +35,13 @@ class Recommender:
             item_bias: Dict[str, float] = json.load(f)
         with open(stats_path, "r") as f:
             stats: Dict[str, float] = json.load(f)
-        with open(interactions_path, "rb") as f:
-            interactions_dict: Dict[str, float] = pickle.load(f)
 
         mean_ratings = stats["mean_rating"]
-        u_name_to_idx = {eval(k)[2]: int(eval(k)[0]) for k, _ in interactions_dict.items()}
-        it_name_to_idx = {eval(k)[3]: int(eval(k)[1]) for k, _ in interactions_dict.items()}
 
-        return mean_ratings, user_factors_mat, item_factors_mat, user_bias, item_bias, u_name_to_idx, it_name_to_idx
+        return mean_ratings, user_factors_mat, item_factors_mat, user_bias, item_bias
 
     def predict(self, user: str, item: str) -> float:
-        u_idx, i_idx = self.u_name_to_idx[user], self.it_name_to_idx[item]
+        u_idx, i_idx = self.ui_indexer(user, item)
         predicted = torch.dot(self.user_factors[u_idx, :], self.item_factors[:, i_idx])
         bias = self.mean_ratings + self.user_bias[str(u_idx)] + self.item_bias[str(i_idx)]
         predicted += bias
---
+++ src/user_item_indexer.py
@@ -0,0 +1,51 @@
+import pickle
+from pathlib import Path
+from typing import Dict
+
+
+class UserItemIndexer:
+    def __init__(self, config):
+        self.cfg = config["DATA"]
+        self.interactions_dict = self.load()
+        self.user_name_to_idx = {eval(k)[2]: int(eval(k)[0]) for k, _ in self.interactions_dict.items()}
+        self.item_name_to_idx = {eval(k)[3]: int(eval(k)[1]) for k, _ in self.interactions_dict.items()}
+
+    def load(self):
+        root_dir = Path(__file__).parent.parent
+        data_inference_dir = root_dir / "data_inference"
+        interactions_path = data_inference_dir / self.cfg.get("interactions_filename")
+
+        with open(interactions_path, "rb") as f:
+            interactions_dict: Dict[str, float] = pickle.load(f)
+
+        return interactions_dict
+
+    def split(self):
+        """
+        At training time
+        """
+        interactions_train = {}
+        interactions_val = {}
+
+        for key, val in self.interactions_dict.items():
+            user_index, item_index, _, _, split = eval(key)
+            if split == "train":
+                interactions_train[f"{(user_index, item_index)}"] = val
+            elif split == "val":
+                interactions_val[f"{(user_index, item_index)}"] = val
+            else:
+                raise ValueError(f"split value {split} is invalid")
+
+        return interactions_train, interactions_val
+
+
+    def __call__(self, user: str, item: str):
+        """
+        At inference time
+        """
+        try:
+            u_idx = self.user_name_to_idx[user]
+            i_idx = self.item_name_to_idx[item]
+        except KeyError:
+            raise KeyError(f"User {user} or item {item} not found in the dataset.")
+        return u_idx, i_idx
