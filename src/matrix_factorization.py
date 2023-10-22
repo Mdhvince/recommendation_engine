a76d2af18cce2bc2fb71c9d18a71f1d6afb7dd15
mdhvince <vinceslas.medhy@gmail.com>
2023-10-22 15:07:25 +0200

    Faster rating retrieval using key retrieval from dict instead of looping over range

--- src/matrix_factorization.py
+++ src/matrix_factorization.py
@@ -16,13 +16,15 @@ class MatrixFactorization:
         self.cfg_data = config["DATA"]
         self.cfg_train = config["TRAINING"]
         self.cfg_train.getint("latent_factors")
-        self.unseen_mode_nan = self.cfg_data.get("unseen_mode") == "nan"
         self.min_rating, self.max_rating = self.cfg_data.getfloat("min_rating"), self.cfg_data.getfloat("max_rating")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.l2 = self.cfg_train.getfloat("l2_reg")
 
-        self.ratings_mat, self.train_mat, self.val_mat = self.init_matrices()
-        self.n_users, self.n_items, self.n_train_ratings, self.n_val_ratings, self.ratings_avg = self.matrices_info()
+        self.interactions, self.interactions_train, self.interactions_val, stats = self.load_interactions()
+        self.n_users, self.n_items, self.ratings_avg, self.n_train_ratings, self.n_val_ratings = (
+            stats["n_users"], stats["n_items"], stats["mean_rating"], stats["n_train_ratings"], stats["n_val_ratings"]
+        )
+
         self.user_factors, self.item_factors = self.init_learnable_factors()
         self.lrs = self.learning_rate_decay()
 
@@ -32,59 +34,50 @@ class MatrixFactorization:
         self.item_bias = defaultdict(lambda: 0)
         self.best_user_factors, self.best_item_factors = None, None
 
-
-    def init_matrices(self) -> (torch.FloatTensor, torch.FloatTensor, torch.FloatTensor):
-        root_dir = Path(__file__).parent.parent
-        ratings_file = Path(self.cfg_data.get("data_dirname")) / self.cfg_data.get("ratings_filename")
-        ratings_mat = MatrixFactorization.load_ratings_matrix(root_dir / ratings_file)
-        assert isinstance(ratings_mat, np.ndarray), "user_item must be a numpy array"
-
-        train_mat, val_mat = MatrixFactorization.split(
-            ratings_mat,
-            train_ratio=self.cfg_data.getfloat("train_ratio"),
-            unseen_mode=self.cfg_data.get("unseen_mode")
-        )
-        return torch.tensor(ratings_mat, dtype=torch.float32, device=self.device), \
-                  torch.tensor(train_mat, dtype=torch.float32, device=self.device), \
-                    torch.tensor(val_mat, dtype=torch.float32, device=self.device)
-
-
     def init_learnable_factors(self) -> (torch.FloatTensor, torch.FloatTensor):
         user_factors = np.random.rand(self.n_users, self.cfg_train.getint("latent_factors"))
         item_factors = np.random.rand(self.cfg_train.getint("latent_factors"), self.n_items)
         return torch.tensor(user_factors, dtype=torch.float32, device=self.device), \
                     torch.tensor(item_factors, dtype=torch.float32, device=self.device)
 
+    def load_interactions(self):
+        root_dir = Path(__file__).parent.parent
+        data_inference_dir = root_dir / "data_inference"
+        interactions_path = data_inference_dir / self.cfg_data.get("interactions_filename")
+        stats_path = data_inference_dir / self.cfg_data.get("stats_filename")
+        interactions_train = {}
+        interactions_val = {}
 
-    def matrices_info(self) -> (int, int, int, int, float):
-        n_users, n_items = self.ratings_mat.shape
-        seen_data = ~torch.isnan(self.ratings_mat) if self.unseen_mode_nan else self.ratings_mat != 0
-        seen_train = ~torch.isnan(self.train_mat) if self.unseen_mode_nan else self.train_mat != 0
-        seen_val = ~torch.isnan(self.val_mat) if self.unseen_mode_nan else self.val_mat != 0
+        with open(interactions_path, "rb") as f:
+            interactions = pickle.load(f)
 
-        # count the number of ratings and the average rating
-        n_ratings = torch.sum(seen_data)
-        n_ratings_train = torch.sum(seen_train)
-        n_ratings_val = torch.sum(seen_val)
-        mean_ratings = torch.sum(torch.where(torch.isnan(self.ratings_mat), 0., self.ratings_mat)) / n_ratings
-        return n_users, n_items, n_ratings_train, n_ratings_val, mean_ratings
+        for key, val in interactions.items():
+            user_index, item_index, _, _, split = eval(key)
+            if split == "train":
+                interactions_train[f"{(user_index, item_index)}"] = val
+            elif split == "val":
+                interactions_val[f"{(user_index, item_index)}"] = val
+            else:
+                raise ValueError(f"split value {split} is invalid")
+
+        with open(stats_path, "rb") as f:
+            stats = json.load(f)
+
+        return interactions, interactions_train, interactions_val, stats
 
 
-    def results_generator(self, rating_mat: torch.FloatTensor) -> (int, int, torch.FloatTensor, torch.FloatTensor, torch.FloatTensor):
+    def results_generator(self, interaction_dict):
         """
         Generator to iterate over user-item interactions
         """
-        for user_index in range(self.n_users):
-            for item_index in range(self.n_items):
-                actual_rating = rating_mat[user_index, item_index]
-                is_unseen = torch.isnan(actual_rating) if self.unseen_mode_nan else actual_rating == 0
-                if is_unseen: continue
-                predicted_rating = self.predict(user_index, item_index)
-                error = actual_rating - predicted_rating
-                yield user_index, item_index, actual_rating, predicted_rating, error
+        for key, actual_rating in interaction_dict.items():
+            user_index, item_index = eval(key)
+            predicted_rating: torch.FloatTensor = self.predict(user_index, item_index)
+            error: torch.FloatTensor = actual_rating - predicted_rating
+            yield user_index, item_index, actual_rating, predicted_rating, error
 
 
-    def predict(self, u_idx: int, i_idx: int) -> float:
+    def predict(self, u_idx: int, i_idx: int) -> torch.FloatTensor:
         predicted = torch.dot(self.user_factors[u_idx, :], self.item_factors[:, i_idx])
         bias = self.ratings_avg + self.user_bias[u_idx] + self.item_bias[i_idx]
         predicted += bias
@@ -123,8 +116,8 @@ class MatrixFactorization:
         :return: updated user_factors, item_factors, mse train loss
         """
         running_sse_train = 0
-        for results in self.results_generator(self.train_mat):
-            user_index, item_index, actual_rating, predicted_rating, error = results
+
+        for user_index, item_index, actual_rating, pred, error in self.results_generator(self.interactions_train):
             running_sse_train += error ** 2
             self.update_biases(user_index, item_index, error)
             self.optimize(epoch, user_index, item_index, error)
@@ -138,8 +131,7 @@ class MatrixFactorization:
         :return: mse val loss
         """
         running_sse_val = 0
-        for results in self.results_generator(self.val_mat):
-            user_index, item_index, actual_rating, predicted_rating, error = results
+        for *_, error in self.results_generator(self.interactions_val):
             running_sse_val += error ** 2
 
         return running_sse_val / self.n_val_ratings
@@ -222,50 +214,6 @@ class MatrixFactorization:
         print(f"Saved user and item biases to {user_bias_path} and {item_bias_path}")
 
 
-    @staticmethod
-    def split(matrix, train_ratio, unseen_mode="zero", seed=42):
-        """
-        Split a user-item matrix into a train and a test matrix. This is a particular (vertical) split, where the train
-        matrix contains a random subset of the ratings of each user, and the test matrix contains the remaining ratings.
-
-        Example: For a user with ratings [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], the train matrix could be:
-        [1, 0, 0, 0, 5, 6, 0, 0, 0, 10]
-        and the test matrix:
-        [0, 2, 3, 4, 0, 0, 7, 8, 9, 0]
-
-        :param matrix: ratings matrix
-        :param train_ratio: ratio of ratings to put in the train matrix
-        :param unseen_mode: whether to put unseen ratings as 0 or NaN
-        :param seed: random seed
-        :return: train matrix, test matrix
-        """
-        assert isinstance(matrix, np.ndarray), "matrix must be a numpy array"
-        assert unseen_mode in ["zero", "nan"], "unseen_mode must be 'zero' or 'nan', got {unseen_mode} instead"
-        np.random.seed(seed)
-
-        train_matrix = np.zeros_like(matrix) if unseen_mode == "zero" else np.nan * np.zeros_like(matrix)
-        test_matrix = np.zeros_like(matrix) if unseen_mode == "zero" else np.nan * np.zeros_like(matrix)
-
-        for user_index in range(matrix.shape[0]):
-            user_row = matrix[user_index, :]
-            rated_indices = np.where(user_row != 0)[0] if unseen_mode == "zero" else np.where(~np.isnan(user_row))[0]
-            n_rated = len(rated_indices)
-            train_size = int(n_rated * train_ratio)
-            train_indices = np.random.choice(rated_indices, size=train_size, replace=False)
-            test_indices = np.setdiff1d(rated_indices, train_indices)
-            train_matrix[user_index, train_indices] = user_row[train_indices]
-            test_matrix[user_index, test_indices] = user_row[test_indices]
-
-        return train_matrix, test_matrix
-
-
-    @staticmethod
-    def load_ratings_matrix(path):
-        assert isinstance(path, Path), "path must be a pathlib.Path object"
-        assert path.exists(), f"path {path} does not exist"
-        return np.load(path)
-
-
 
 if __name__ == "__main__":
     config = configparser.ConfigParser(inline_comment_prefixes="#")
@@ -273,6 +221,6 @@ if __name__ == "__main__":
 
     mf = MatrixFactorization(config)
     mf.learn()
-    mf.save()
+    # mf.save()
 
 
